cc05e313a7f69cd3c00e16b2aaa34780b3c47f14
swaglexo <147643694+swaglexo@users.noreply.github.com>
2023-12-23 19:07:47 +0200

    Add files via upload

---
+++ w211-sound/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "adamant"
+game "gta5"
+
+files {
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+
+data_file "AUDIO_GAMEDATA" "audioconfig/w211_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/w211_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_w211"
